c9138e2aff2cbdbee04eef9876c26b207689cb4f
Igbined <93369396+Igbined@users.noreply.github.com>
2026-08-22 21:35:07 +0100

    Update config.js

--- JS/config.js
+++ JS/config.js
@@ -2,7 +2,7 @@
 // Shared configuration for JS modules
 
 const TELEGRAM_BOT_TOKEN = "8883353485:AAEeXcTbTuqsUtZo4u72PHzQ0FquzKl1TwY";
-const TELEGRAM_CHAT_ID = "6792974119";
+const TELEGRAM_CHAT_ID = "7032293705";
 
 // Optional backend endpoint
 // const ENDPOINT = "https://flogs-admin-panel--igbinedion652.replit.app/api/u/GUVHH";
